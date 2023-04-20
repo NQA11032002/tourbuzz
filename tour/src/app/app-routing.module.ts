49b5919d9d1a1857aa9ca8f76b207f3c463773cc
NQA11032002 <kawasakisga@gmail.com>
2023-04-20 17:58:48 +0700

    merge success

--- tour/src/app/app-routing.module.ts
+++ tour/src/app/app-routing.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-const routes: Routes = [];
-=======
-import { LoginComponent } from './component/author/login/login.component';
-import { RegisterComponent } from './component/author/register/register.component';
-import { ForgotpassComponent } from './component/author/forgotpass/forgotpass.component';
+
+import { LoginComponent } from './component/auth/login/login.component';
+import { RegisterComponent } from './component/auth/register/register.component';
+import { ForgotpassComponent } from './component/auth/forgotpass/forgotpass.component';
 
 const routes: Routes = [
   {path:'login',component: LoginComponent},
   {path:'register', component: RegisterComponent},
   {path:'forgotpass', component: ForgotpassComponent},
 ];
->>>>>>> 2d6b220aee9050767e996e83c4637326954991f3
-=======
-
-const routes: Routes = [];
->>>>>>> son
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
