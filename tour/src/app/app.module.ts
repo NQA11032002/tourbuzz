bdfcc976bf1b97f19d3b51edb38c0fb016b4a8ca
HoQuangDat <118000757+HoQuangDat@users.noreply.github.com>
2023-04-20 17:00:12 +0700

    Them component login, register, forgotpassw, navbar, chuyen trang

--- tour/src/app/app.module.ts
+++ tour/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { LoginComponent } from './component/author/login/login.component';
+import { RegisterComponent } from './component/author/register/register.component';
+import { ForgotpassComponent } from './component/author/forgotpass/forgotpass.component';
+import { NavbarComponent } from './component/social/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotpassComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
