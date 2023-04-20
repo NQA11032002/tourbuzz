3338291ae0640a36f7612e0d2fd9c8da80e61ad3
NQA11032002 <kawasakisga@gmail.com>
2023-04-20 17:03:26 +0700

    update post and create post

--- tour/src/app/app.module.ts
+++ tour/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { PostComponent } from './component/social/post/post.component';
+import { CreatePostComponent } from './component/social/create-post/create-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    CreatePostComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
