50102746bf02f1b4ee563d9f3d32deabc4961805
Nguyễn Quốc Anh <kawasakisga@gmail.com>
2023-04-21 00:24:44 +0700

    update component post, create post, messenger, fix client header, sidebar

--- tour/src/app/app.module.ts
+++ tour/src/app/app.module.ts
@@ -7,12 +7,13 @@ import { HeaderComponent } from './component/blocks/header/header.component';
 import { FooterComponent } from './component/blocks/footer/footer.component';
 
 import { NavbarComponent } from './component/social/navbar/navbar.component';
-import { NavbarTourComponent } from './component/tour/navbar-tour/navbar-tour.component';
 import { CreatePostComponent } from './component/social/create-post/create-post.component';
 import { PostComponent } from './component/social/post/post.component';
 import { LoginComponent } from './component/auth/login/login.component';
 import { RegisterComponent } from './component/auth/register/register.component';
 import { ForgotpassComponent } from './component/auth/forgotpass/forgotpass.component';
+import { NavbarTourComponent } from './component/tours/navbar-tour/navbar-tour.component';
+import { MessengerComponent } from './component/blocks/messenger/messenger.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { ForgotpassComponent } from './component/auth/forgotpass/forgotpass.comp
     ForgotpassComponent,
     NavbarComponent,
     NavbarTourComponent,
+    MessengerComponent,
   ],
   imports: [
     BrowserModule,
