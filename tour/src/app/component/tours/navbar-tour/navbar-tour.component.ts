ad0388a9a2ff4fa181dcdf39886e16004d09b6a8
kuro1417 <nson659@gmail.com>
2023-05-06 18:19:38 +0700

    them keo gia o navbartour

--- tour/src/app/component/tours/navbar-tour/navbar-tour.component.ts
+++ tour/src/app/component/tours/navbar-tour/navbar-tour.component.ts
@@ -1,8 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { AddressService } from 'src/app/services/address.service';
 
 @Component({
   selector: 'app-navbar-tour',
   templateUrl: './navbar-tour.component.html',
   styleUrls: ['./navbar-tour.component.scss']
 }) 
-export class NavbarTourComponent{}
+export class NavbarTourComponent implements OnInit {
+  public data: Array<any> = new Array<any>();
+  public rangeValue: number = 500000;
+
+  public constructor(private address:AddressService){}
+  ngOnInit(): void {
+    this.getCity();
+  }
+
+  getCity(){
+    let token = sessionStorage.getItem('token_user');
+    
+    if(token != null){
+      this.address.getCities(token).subscribe(p=>{
+        this.data = p.data;
+      })
+    }
+  }
+
+  onRangeValueChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.rangeValue = parseInt(value);
+    const priceStart = document.querySelector('.price') as HTMLSpanElement;
+    priceStart.innerText = `${this.rangeValue}`;
+  }
+}
